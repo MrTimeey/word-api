872e69d7442537e5e11f45408489ce04d3a1ef73
Tim Siegler <tim.siegler@fincon.eu>
2021-05-14 09:38:17 +0200

    Add test for default language

--- test/api/words/create.js
+++ test/api/words/create.js
@@ -38,20 +38,39 @@ describe('POST /api/words', () => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('message').be.eql('Word created!');
-                    console.log('HIER', res.body);
                     res.body.should.have.property('word').should.be.not.empty;
-                    /*Should contain language en*/
+               });
+          /* Check if default language was added */
+          chai.request(app)
+               .get('/api/words')
+               .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').be.eql('Words found!');
+                    res.body.should.have.property('words').should.be.not.empty;
+                    const { words } = res.body;
+                    expect(words).to.have.lengthOf(1);
+                    const createdWord = words[0];
+                    expect(createdWord)
+                         .to.have.property('language')
+                         .be.eql('en');
+                    expect(createdWord)
+                         .to.have.property('word')
+                         .be.eql('Aquiver');
+                    expect(createdWord)
+                         .to.have.property('definition')
+                         .be.eql('(adj.) Quivering, trembling');
                     done();
                });
      });
      it('Creation failed', done => {
-          let dino = {
+          let wordObj = {
                word: 'Mellifluous',
           };
           chai.request(app)
                .post('/api/words')
                .set('content-type', 'application/json')
-               .send(dino)
+               .send(wordObj)
                .end((err, res) => {
                     res.should.have.status(400);
                     const { body } = res;
